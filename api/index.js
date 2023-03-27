d52908081828b06c3343ada5ac251c19a77f20b5
nakiche <tommynaquiche@hotmail.com>
2023-03-26 21:35:20 -0500

    dejando todo como antes de intentar deploy

--- api/index.js
+++ api/index.js
@@ -17,16 +17,14 @@
 //     =====`-.____`.___ \_____/___.-`___.-'=====
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-require('dotenv').config();
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const saveApiData = require ('./src/controllers/saveApiData.js');
-const PORT = process.env.PORT || 3001
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(async() => {console.log('db connected')
     await saveApiData();
-    server.listen(PORT, () => {
-    console.log(`server listening at ${PORT}`); // eslint-disable-line no-console
+    server.listen(3001, () => {
+    console.log('server listening at 3001'); // eslint-disable-line no-console
   });
 });
